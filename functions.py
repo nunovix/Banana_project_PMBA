c640e3b8a4ff4a79846289e29ad88e1650599603
tiagojosemiranda <76543249+tiagojosemiranda@users.noreply.github.com>
2023-06-02 19:15:11 +0100

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+import os
+
+# function to transform image to be used in cnn
+# takes an image
+# returns the transformed image
+
+def transform(og):
+    t = cv2.resize(og, (64, 64))
+    t = cv2.cvtColor(t, cv2.COLOR_RGB2HSV) # BGR to RGB 
+
+    hsv_max = [179, 255, 255]
+
+    t = np.divide(t, hsv_max)
+
+    return t
